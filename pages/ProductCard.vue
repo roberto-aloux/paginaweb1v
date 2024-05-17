9c9232a0339579c00e8696096e826298827ed5ad
vanyx2002 <yovannycorteslezama2018@gmail.com>
2024-05-17 16:01:58 -0600

    Nuevo repositorio

---
+++ pages/ProductCard.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="border-2 border-purple-500 p-4 rounded-lg">
+      <img src="https://placehold.co/300x180" alt="Asus ROG Keris Wireless Gaming Mouse Black" class="mb-4">
+      <h3 class="text-lg font-semibold text-zinc-900 dark:text-white">Asus ROG Keris Wireless Gaming Mouse Black</h3>
+      <p class="text-zinc-700 dark:text-zinc-300 mb-4">$500 MXN</p>
+      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Saber Más</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  };
+  </script>
